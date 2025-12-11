5de3a3827436d47c52bf61f797191952186e2745
TimoSci <me@timosci.net>
2025-12-11 02:53:33 +0100

    move params to config.toml file

--- play.jl
+++ play.jl
@@ -1,4 +1,7 @@
+import TOML
 using PortAudio
+
+cfg = TOML.parsefile("config.toml")
 
 function find_device(name_substring)
     devices = PortAudio.devices()
@@ -10,11 +13,11 @@ function find_device(name_substring)
     error("Device with name containing '$name_substring' not found.")
 end
 
-function play(base_freq=440, duration=2)
+function play(base_freq=cfg["app"]["base_freq"], duration=cfg["app"]["duration"])
     device = find_device("MacBook Air Speakers") # Replace with actual substring of your device nam
     # device = PortAudio.devices()[4]
     S = 44100 # sampling rate (samples / second)
-    x = cos.(2pi*(1:2S)*440/S) + cos.(2pi*(1:2S)*660/S) # A440 tone for 2 seconds
+    x = cos.(2pi*(1:2S)*base_freq/S) + cos.(2pi*(1:2S)*base_freq*1.5/S) # A440 tone for 2 seconds
     PortAudioStream(device,0, 2; samplerate=S) do stream
            write(stream, x)
     end
