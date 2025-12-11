4f7f91b220df15921464254967b1c84bcc0c1873
TimoSci <me@timosci.net>
2025-12-11 17:41:53 +0100

    set sample rate in config file

--- play.jl
+++ play.jl
@@ -17,10 +17,9 @@ function find_device(name_substring)
 end
 
 
-function play(base_freq=cfg["app"]["base_freq"], duration=cfg["app"]["duration"], amplitude_envelope=amplitude_envelope_linear)
+function play(base_freq=cfg["app"]["base_freq"], duration=cfg["app"]["duration"], amplitude_envelope=amplitude_envelope_linear, sample_rate=cfg["app"]["sample_rate"])
     device = find_device("MacBook Air Speakers") # Replace with actual substring of your device nam
-    # device = PortAudio.devices()[4]
-    S = 44100 # sampling rate (samples / second)
+    S = sample_rate # sampling rate (samples / second)
     ns = duration * S # total number of samples
     x = amplitude_envelope(2ns) .* cos.(2pi*(1:2ns)*base_freq/S) #+ cos.(duration*pi*(1:ns)*base_freq*1.5/S) # A440 tone for 2 seconds
     PortAudioStream(device,0, 2; samplerate=S) do stream
